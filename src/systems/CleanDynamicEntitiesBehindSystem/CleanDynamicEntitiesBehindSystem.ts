1ab7983c47fe0b521bc3aacd1584348180922e75
Mateusz Cybulski <mateuszcybulski@MacBook-Pro-Mateusz.local>
2026-02-07 11:35:28 +0100

    add guard to CleanDynamicEntitiesBehindSystem so ThrowBallSystem works properly

--- src/systems/CleanDynamicEntitiesBehindSystem/CleanDynamicEntitiesBehindSystem.ts
+++ src/systems/CleanDynamicEntitiesBehindSystem/CleanDynamicEntitiesBehindSystem.ts
@@ -1,9 +1,16 @@
-import MeshSplitter from '@src/components/MeshSplitter';
+import Dynamic from '@src/components/Dynamic';
 import Transform from '@src/components/Transform';
 import type Entity from '@src/core/Entity';
 import System from '@src/core/System';
 import autoBind from 'auto-bind';
 import * as THREE from 'three';
+
+/**
+ * // TODO Apply Dynamic only on items that require it.
+ * Right now newly thrown ball has position Z of camera
+ * what removes it immediately after camera moves.
+ */
+const SAFE_DISTANCE_BOUND = 5;
 
 export default class extends System {
   constructor(private camera: THREE.Camera) {
@@ -12,14 +19,14 @@ export default class extends System {
   }
 
   update(): void {
-    this.query(Transform, MeshSplitter).forEach(this.removeEntitiesBehind);
+    this.query(Dynamic).forEach(this.removeEntitiesBehind);
   }
 
   private removeEntitiesBehind(entity: Entity) {
     const transform = entity.get(Transform);
     if (!transform) return;
 
-    if (transform.position.z > this.camera.position.z) {
+    if (transform.position.z > this.camera.position.z + SAFE_DISTANCE_BOUND) {
       this.world.destroyEntity(entity);
     }
   }
