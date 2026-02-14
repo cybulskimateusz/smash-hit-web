9edd3ea6ea675f0b83bfddae4d8b612d25d41564
Mateusz Cybulski <mateuszcybulski@macbook-pro-mateusz-1.home>
2026-02-14 18:29:44 +0100

    fix deploy

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -1,7 +1,7 @@
 resource "google_artifact_registry_repository" "docker" {
   project       = google_project.smash_hit.project_id
   location      = var.region
-  repository_id = "smash-hit"
+  repository_id = "smash-hit-web"
   format        = "DOCKER"
   description   = "Docker images for Smash Hit Web"
 
