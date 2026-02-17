b87794626e2bc4c796a01e4c88d1c08f744cfcec
Mateusz Cybulski <mateuszcybulski@macbook-pro-mateusz-1.home>
2026-02-17 05:09:43 +0100

    get rid of entrypoint.sh

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-if [ -n "$WS_URL" ]; then
-  echo "Replacing WS_URL placeholder with: $WS_URL"
-  find /app/dist -name '*.js' -exec sed -i "s|__WS_URL_PLACEHOLDER__|${WS_URL}|g" {} +
-fi
-
-exec serve -s dist -l ${PORT:-8080}
