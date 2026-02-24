4456b44ab58789a519c1c94fff7d789c8e32138c
Mateusz Cybulski <mateuszcybulski@macbook-pro-mateusz-2.home>
2026-02-24 09:38:21 +0100

    redesign + improvements

--- src/desktop/index.ts
+++ src/desktop/index.ts
@@ -1,22 +1,21 @@
 import autoBind from 'auto-bind';
 
 import GameView from './views/game-view/game-view';
-// import QRCodeView from './views/qr-code-view/qr-code-view';
+import QRCodeView from './views/qr-code-view/qr-code-view';
 
 class Desktop {
   private app: HTMLElement = document.querySelector('#app')!;
-  // private qrCodeView: QRCodeView;
+  private qrCodeView: QRCodeView;
 
   constructor() {
     autoBind(this);
 
-    // this.qrCodeView = new QRCodeView({ onGameStart: this.onGameStart });
-    // this.app.appendChild(this.qrCodeView.view);
-    this.onGameStart();
+    this.qrCodeView = new QRCodeView({ onGameStart: this.onGameStart });
+    this.app.appendChild(this.qrCodeView.view);
   }
 
   private onGameStart() {
-    // this.qrCodeView.view.remove();
+    this.qrCodeView.view.remove();
     this.app.appendChild(new GameView().view);
   }
 }
